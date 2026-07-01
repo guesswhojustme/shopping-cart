4581ae58cf80c403cbf50d731d6441dae2cf79fe
guesswhojustme <imnayeonsaranghaee@gmail.com>
2026-07-01 14:37:12 +0800

    add home page content

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,18 @@
+import styles from './HomePage.module.css';
+import { Link } from 'react-router-dom';
 
 export function HomePage(){
     return(
-        <div>
-            <h1>This is the Home Page</h1>
+        <div className={styles.container}>
+            <h1>We sell Pokemons</h1>
+            <div className={styles.mid}>
+                <span>Catch em all? ❌ Buy em all? ✅</span>
+                <div>
+                    <p>Tired of wasting pokeballs and walking around grasses to catch pokemon?</p>
+                    <p>Dont worry we got you!</p>
+                </div>
+                <span><strong>Buy now!</strong></span>
+            </div>
         </div>
     )
 }
