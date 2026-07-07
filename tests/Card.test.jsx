1d827fa4c23d2ae964ad0b983d7720b166d54fc1
guesswhojustme <imnayeonsaranghaee@gmail.com>
2026-07-07 10:47:05 +0800

    add card component test
    - test rendered button
    - test rendered button handler via mocking

--- tests/Card.test.jsx
+++ tests/Card.test.jsx
@@ -1,13 +1,19 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event"
 import { Card } from "../src/components/Card/Card.jsx";
 import { type } from "@testing-library/user-event/dist/cjs/utility/type.js";
 
+
+
+
 describe("Card component test", () => {
-    it("renders pokemon's correct data: id, name, type, weight, height, hp, img", () => {
+    const onClick = vi.fn();
+    const user = userEvent.setup();
+
+    it("renders pokemon's correct data: id, name, type, weight, height, hp, price, img", () => {
         
-        render(<Card />)
+        render( <Card pokemon={'bulbasaur'}/>)
 
         const img = screen.getByRole("img", {name: "ditto"})
         expect(screen.getByText(`#132`));
@@ -18,5 +24,65 @@ describe("Card component test", () => {
         expect(screen.getByText('48'));
         expect(screen.getByText('$0'));
         expect(img).toHaveAttribute("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png");
+    })
+
+    it("remove button rendered", () => {
+        render(<Card />);
+        expect(screen.getByRole("button", {name: /remove/i})).toBeInTheDocument();
+    })
+
+    it("add to cart button rendered", () => {
+        render(<Card page={'shop'}/>);
+        expect(screen.getByRole("button", {name: /add to cart/i})).toBeInTheDocument();
+    })
+
+    it("decrease - button rendered", () => {
+        render(<Card />);
+        expect(screen.getByRole("button", {name: '-'})).toBeInTheDocument();
+    })
+
+    it("inrease + button rendered", () => {
+        render(<Card />);
+        expect(screen.getByRole("button", {name: '+'})).toBeInTheDocument();
+    })
+
+    it("remove button handler fires", async () => {
+        render(<Card removeToCart={onClick}/>);
+
+        const btn = screen.getByRole("button", {name: /remove/i});
+
+        await user.click(btn);
+
+        expect(onClick).toHaveBeenCalled();
+    })
+
+    it("add to cart button handler fires", async () => {
+        render(<Card page={'shop'} addToCart={onClick}/>);
+
+        const btn = screen.getByRole("button", {name: /add to cart/i});
+
+        await user.click(btn);
+
+        expect(onClick).toHaveBeenCalled();
+    })
+    
+    it("decrease - button handler fires", async () => {
+        render(<Card handleDecreaseState={onClick}/>);
+
+        const btn = screen.getByRole("button", {name: '-'});
+
+        await user.click(btn);
+
+        expect(onClick).toHaveBeenCalled();
+    })
+
+    it("increase + button handler fires", async () => {
+        render(<Card handleIncreaseState={onClick}/>);
+
+        const btn = screen.getByRole("button", {name: '+'});
+
+        await user.click(btn);
+
+        expect(onClick).toHaveBeenCalled();
     })
 })
